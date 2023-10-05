da2a4a888a34b337017e3f87eb1985415fea7591
Francisco Egenau Navia <fr.egenau@duocuc.cl>
2023-10-04 23:40:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Duoc-Asistencia',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
